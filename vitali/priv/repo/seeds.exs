dca4892c73d1fa4f852eda85e707c91d6118bd53
Daniel Pinheiro <daniel@danieldev.io>
2024-02-13 13:43:45 -0300

    add seeds

--- vitali/priv/repo/seeds.exs
+++ vitali/priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# email and password for each user
+users = [
+  %{email: "batman@example.com", password: "password"},
+  %{email: "wolverine@example.com", password: "password"},
+  %{email: "hulk@example.com", password: "password"},
+  %{email: "drmanhattan@example.com", password: "password"},
+  %{email: "ironman@example.com", password: "password"}
+  ]
+
+Enum.map(users, fn user -> Vitali.Accounts.User.register_user(%Vitali.Accounts.User{}, user) end)
