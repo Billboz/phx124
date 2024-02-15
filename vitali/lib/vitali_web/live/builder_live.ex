fbdb533108ed5af6e9f8847c2de65c8b9a453d89
Bill Boz <Billboz1@gmail.com>
2024-02-15 13:46:56 -0500

    created board

--- vitali/lib/vitali_web/live/builder_live.ex
+++ vitali/lib/vitali_web/live/builder_live.ex
@@ -1,14 +1,17 @@
 defmodule VitaliWeb.BuilderLive do
   # alias Vitali.Accounts
   # alias Vitali.Accounts.User
+  import VitaliWeb.CoreComponents
 
   use VitaliWeb, :live_view
 
   def render(assigns) do
     ~H"""
     <h1 class="text-4xl text-center">Builder</h1>
-    <svg width="400" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect width="200" height="100" x="100" y="50" rx="20" ry="20" fill="blue" />
+    <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">
+      <%= for x <- 0..40, y <- 0..40 do %>
+        <.life_cell x={x} y={y} live={Enum.random([true, false])}/>
+      <% end %>
     </svg>
     """
   end
